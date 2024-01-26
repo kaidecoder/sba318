e8fc44a8c8ccca01683a49b382908739fca7db75
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2024-01-25 22:16:47 -0500

    combined the /food routes to get all foods and perform the zip query as the last listed route was the only one being hit

--- routes/food.js
+++ routes/food.js
@@ -1,95 +1,81 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const Food = require("../models/food");
 
 //about the app
 router.get("/about", (req, res) => {
-    res.render("about");
-  });
-  
-  
-  //create a new food in the form
-  router.get("/create", (req, res) => {
-    res.render("createResource");
-  });
+  res.render("about");
+});
 
-//get all the food
-router.get("/food", (req, res) => {
-    Food.find()
-      .then((result) => {
-        res.render("index", { foods: result });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
+//create a new food in the form
+router.get("/create", (req, res) => {
+  res.render("createResource");
+});
 
-  //query food with zip=02116
 router.get("/food", async (req, res) => {
-  const zip = req.query.zip
-  Food.find({zip})
-  .then(doc => {
-      res.status(200).json(doc)
-  })
-  .catch(error => {
-      res.status(500).json({error: "document not available"})
-  })
-})
+  try {
+    // Retrieve foods based on the ZIP code (if provided)
+    const zip = req.query.zip;
+    const foods = zip ? await Food.find({ zip: zip }) : await Food.find();
+
+    // Render the "index" page with the list of foods
+    res.render("index", { foods: foods });
+  } catch (error) {
+    console.error(error); // Log the error for debugging
+    res.status(500).json({ error: "document not available" });
+  }
+});
 
 
-  
-  //add a new food
-  router.post("/food", (req, res) => {
-    const food = new Food(req.body);
-  
-    food
-      .save()
-      .then((result) => {
-        res.redirect("/food");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
-  
-  //get one food - send to showpage
-  router.get("/food/:id", async (req, res) => {
+//add a new food
+router.post("/food", (req, res) => {
+  const food = new Food(req.body);
+
+  food
+    .save()
+    .then((result) => {
+      res.redirect("/food");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//get one food - send to showpage
+router.get("/food/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  await Food.findById(id)
+    .then((result) => {
+      console.log(result);
+      res.render("showPage", { food: result });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//delete a food item
+router.delete("/food/:id", (req, res) => {
+  const id = req.params.id;
+  Food.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/food" });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+//update a food item
+router.patch("/food/:id", async (req, res) => {
+  try {
     const id = req.params.id;
-    console.log(id);
-    await Food.findById(id)
-      .then((result) => {
-        console.log(result);
-        res.render("showPage", { food: result });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
-  
-  //delete a food item
-  router.delete("/food/:id", (req, res) => {
-    const id = req.params.id 
-    Food.findByIdAndDelete(id)
-    .then(result => {
-      res.json({redirect: "/food"})
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  })
-  
-  //update a food item
-  router.patch("/food/:id", async (req, res) => {
-    try {
-      const id = req.params.id 
-      const food = await Food.findByIdAndUpdate(id, req.body, {new: true})
-      console.log(food)
-    
-      res.json({food})
-    } catch (error) {
-      
-    }
-  
-  })
+    const food = await Food.findByIdAndUpdate(id, req.body, { new: true });
+    console.log(food);
 
-module.exports = router
+    res.json({ food });
+  } catch (error) {}
+});
+
+module.exports = router;
